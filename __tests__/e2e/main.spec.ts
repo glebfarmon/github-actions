a5bf7678f9119ad0ccae98f837f5752e00b631e4
Hlib Ilnytsky <glebfarmon@gmail.com>
2023-09-03 13:46:52 +0200

    Fix e2e

--- __tests__/e2e/main.spec.ts
+++ __tests__/e2e/main.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({page}) => {
   await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('r1j3irjfasnfo')
+  await expect(page).toHaveTitle(/Playwright/)
 })
 
 test('get started link', async ({page}) => {
